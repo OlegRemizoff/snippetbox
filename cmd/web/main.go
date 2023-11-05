d26ae0061d4c3cfd948e865680a36fe8e44d49ca
Oleg Remizov <job.rem1zoff@gmail.com>
2023-11-05 14:58:42 +0300

    static files

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	// "path/filepath"
 )
 
 
@@ -17,6 +18,12 @@ func main() {
 	r.HandleFunc("/snippet/create", createSnippet)
 
 
+	// Регистрации обработчика для всех запросов, которые начинаются с "/static/"
+	// Убираем префикс "/static" перед тем как запрос достигнет http.FileServer
+	fileServer := http.FileServer(http.Dir("./ui/static"))
+	r.Handle("/static/", http.StripPrefix("/static", fileServer))
+
+
 	// Используется функция http.ListenAndServe() для запуска нового веб-сервера. 
     // Мы передаем два параметра: TCP-адрес сети для прослушивания и созданный роутер
 	log.Println("Запуск сервера на http://127.0.0.1:8000")
@@ -24,3 +31,43 @@ func main() {
 	log.Fatal(err)
 
 }
+
+
+
+
+
+
+
+// Ограничение просмотра файловой системы
+// type neuteredFileSystem struct {
+// 	fs http.FileSystem
+// }
+
+// func (nfs neuteredFileSystem) Open(path string) (http.File, error) {
+// 	f, err := nfs.fs.Open(path)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+
+// 	s, err := f.Stat()
+// 	if s.IsDir() {
+// 		index := filepath.Join(path, "index.html")
+// 		if _, err := nfs.fs.Open(index); err != nil {
+// 			closeErr := f.Close()
+// 			if closeErr != nil {
+// 				return nil, closeErr
+// 			}
+
+// 			return nil, err
+// 		}
+// 	}
+
+// 	return f, nil
+// }
+
+
+// main () {
+// 	fileServer := http.FileServer(neuteredFileSystem{http.Dir("./static")})
+//     mux.Handle("/static", http.NotFoundHandler())
+//     mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+// }
